1cff16df6b6aa8e7b7a84c55d85305cbb61efca2
Cai Huijian <hjcai@telenav.cn>
2021-07-19 20:19:58 +0800

    remove ButterKnife

--- d05selfbutterknife/src/main/java/com/example/selfbutterknife/MainApplication.java
+++ d05selfbutterknife/src/main/java/com/example/selfbutterknife/MainApplication.java
@@ -1,13 +1,7 @@
 package com.example.selfbutterknife;
 
-import android.app.Activity;
 import android.app.Application;
-import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-import butterknife.ButterKnife;
 
 /**
  * Created by hjcai on 2021/7/16.
