ae5cf6dfe38ffa7fb2aee0d1df67ccfa80911c5b
Cai Huijian <hjcai@telenav.cn>
2021-07-19 21:11:17 +0800

    Revert "remove ButterKnife"
    This reverts commit ac994ec5

--- d05selfbutterknife/src/main/java/com/example/selfbutterknife/Activity2.java
+++ d05selfbutterknife/src/main/java/com/example/selfbutterknife/Activity2.java
@@ -11,21 +11,27 @@ import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 public class Activity2 extends AppCompatActivity {
+    @BindView(R.id.tv1)
     TextView mTextView;
 
+    @BindView(R.id.button1)
     Button mButton;
+
+    Unbinder unbinder = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        unbinder = ButterKnife.bind(this);
 
-        //mTextView.setText("Activity2-butter-tv2");
-        //mButton.setText("Activity2-butter-button2");
+        mTextView.setText("Activity2-butter-tv2");
+        mButton.setText("Activity2-butter-button2");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        unbinder.unbind();
     }
 }
--- d05selfbutterknife/src/main/java/com/example/selfbutterknife/MainActivity.java
+++ d05selfbutterknife/src/main/java/com/example/selfbutterknife/MainActivity.java
@@ -14,18 +14,27 @@ import butterknife.OnClick;
 import butterknife.Unbinder;
 
 public class MainActivity extends AppCompatActivity {
+    // 3.ButterKnife属性初始化 注意View必须有id
+    @BindView(R.id.tv1)
     TextView mTextView;
 
+    @BindView(R.id.button1)
     Button mButton;
+    Unbinder unbinder = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //mTextView.setText("Activity1-butter-tv1");
-        //mButton.setText("Activity1-butter-button1");
+        //2 初始化ButterKnife
+        unbinder = ButterKnife.bind(this);
+        //4 ButterKnife属性使用
+        mTextView.setText("Activity1-butter-tv1");
+        mButton.setText("Activity1-butter-button1");
     }
 
+    //5 ButterKnife Event使用
+    @OnClick(R.id.button1)
     void buttonClick() {
         Intent intent = new Intent(MainActivity.this,Activity2.class);
         startActivity(intent);
@@ -34,5 +43,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        // 6.调用unbind取消ButterKnife的注入
+        unbinder.unbind();
     }
 }
