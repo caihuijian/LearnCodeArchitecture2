e83d75d5870cc354a6b6d4687364050516d46dde
Cai Huijian <hjcai@telenav.cn>
2021-08-04 09:29:18 +0800

    单例架子

---
+++ d08singleton/src/main/java/com/example/d08singleton/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.d08singleton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
